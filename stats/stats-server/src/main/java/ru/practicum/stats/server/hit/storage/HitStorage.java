1bc835a9c4737e0fcb6ce382665a44e0fe364c0c
Alexander <ssddds73@gmail.com>
2023-05-18 07:45:11 +0300

    <fix> Исправлены ошибки

--- stats/stats-server/src/main/java/ru/practicum/stats/server/hit/storage/HitStorage.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/hit/storage/HitStorage.java
@@ -10,33 +10,33 @@ import java.util.List;
 
 public interface HitStorage extends JpaRepository<Hit, Integer> {
 
-    @Query("SELECT new ru.practicum.stats.server.hit.model.Stats(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
-            "FROM Hits AS s " +
-            "WHERE s.timestamp BETWEEN ?1 AND ?2 " +
-            "GROUP BY s.app, s.uri " +
-            "ORDER BY COUNT(DISTINCT s.ip) DESC")
+    @Query("SELECT new ru.practicum.stats.server.hit.model.Stats(hit.app, hit.uri, COUNT(DISTINCT hit.ip)) " +
+            "FROM Hit hit " +
+            "WHERE hit.timestamp BETWEEN ?1 AND ?2 " +
+            "GROUP BY hit.app, hit.uri " +
+            "ORDER BY COUNT(DISTINCT hit.ip) DESC")
     List<Stats> getAllStatsDistinctIp(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.stats.server.hit.model.Stats(s.app, s.uri, COUNT(s.ip)) " +
-            "FROM Hits AS s " +
-            "WHERE s.timestamp BETWEEN ?1 AND ?2 " +
-            "GROUP BY s.app, s.uri " +
-            "ORDER BY COUNT(s.ip) DESC")
+    @Query("SELECT new ru.practicum.stats.server.hit.model.Stats(hit.app, hit.uri, COUNT(hit.ip)) " +
+            "FROM Hit hit " +
+            "WHERE hit.timestamp BETWEEN ?1 AND ?2 " +
+            "GROUP BY hit.app, hit.uri " +
+            "ORDER BY COUNT(hit.ip) DESC")
     List<Stats> getAllStats(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.stats.server.hit.model.Stats(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
-            "FROM Hits AS s " +
-            "WHERE s.timestamp BETWEEN ?1 AND ?2 " +
-            "AND s.uri IN (?3) " +
-            "GROUP BY s.app, s.uri " +
-            "ORDER BY COUNT(DISTINCT s.ip) DESC")
+    @Query("SELECT new ru.practicum.stats.server.hit.model.Stats(hit.app, hit.uri, COUNT(DISTINCT hit.ip)) " +
+            "FROM Hit hit " +
+            "WHERE hit.timestamp BETWEEN ?1 AND ?2 " +
+            "AND hit.uri IN (?3) " +
+            "GROUP BY hit.app, hit.uri " +
+            "ORDER BY COUNT(DISTINCT hit.ip) DESC")
     List<Stats> getStatsByUrisDistinctIp(LocalDateTime start, LocalDateTime end, List<String> uri);
 
-    @Query("SELECT new ru.practicum.stats.server.hit.model.Stats(s.app, s.uri, COUNT(s.ip)) " +
-            "FROM Hits AS s " +
-            "WHERE s.timestamp BETWEEN ?1 AND ?2 " +
-            "AND s.uri IN (?3) " +
-            "GROUP BY s.app, s.uri " +
-            "ORDER BY COUNT(s.ip) DESC")
+    @Query("SELECT new ru.practicum.stats.server.hit.model.Stats(hit.app, hit.uri, COUNT(hit.ip)) " +
+            "FROM Hit hit " +
+            "WHERE hit.timestamp BETWEEN ?1 AND ?2 " +
+            "AND hit.uri IN (?3) " +
+            "GROUP BY hit.app, hit.uri " +
+            "ORDER BY COUNT(hit.ip) DESC")
     List<Stats> getStatsByUris(LocalDateTime start, LocalDateTime end, List<String> uri);
 }
