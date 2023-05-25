be24434387fb72d3dadcb5235e45042ecd740a04
Alexander <ssddds73@gmail.com>
2023-05-25 15:16:29 +0300

    <fix> исправлены ошибки

--- stats/stats-server/src/main/java/ru/practicum/hit/service/HitServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/hit/service/HitServiceImpl.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.hit.exception.StatsBadTimeException;
 import ru.practicum.hit.mapper.HitMapper;
 import ru.practicum.hit.mapper.StatsMapper;
 import ru.practicum.hit.storage.StatsStorage;
